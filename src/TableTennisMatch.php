899857ed8c7cbc77798ab70a80b439e487f18191
Daniele Megna <daniele.megna@nyxsolutions.it>
2014-07-04 14:12:30 +0200

    Added tests about some points

--- src/TableTennisMatch.php
+++ src/TableTennisMatch.php
@@ -2,5 +2,40 @@
 
 class TableTennisMatch
 {
+  private $player1Name;
+  private $player2Name;
 
+  private $player1Points;
+  private $player2Points;
+
+  public function __construct($player1Name, $player2Name)
+  {
+    $this->player1Name = $player1Name;
+    $this->player2Name = $player2Name;
+
+    $this->player1Points = 0;
+    $this->player2Points = 0;
+  }
+
+  public function printStatus()
+  {
+    $nextPlayer = 
+      (((($this->player1Points + $this->player2Points) / 5) % 2) == 0) ?
+      $this->player1Name : $this->player2Name;
+      
+    return sprintf(
+      "[%d - %d] Ball to %s",
+      $this->player1Points,
+      $this->player2Points,
+      $nextPlayer
+    );
+  }
+
+  public function point($playerName)
+  {
+    if($playerName == $this->player1Name)
+      $this->player1Points++;
+    else
+      $this->player2Points++;
+  }
 }
--- tests/acceptance/TableTennisRankTest.php
+++ tests/acceptance/TableTennisRankTest.php
@@ -2,19 +2,53 @@
 
 class TableTennisRankTest extends PHPUnit_Framework_TestCase
 {
+  private $match;
+  
+  private function initMatch($p1, $p2)
+  {
+    $this->match = new TableTennisMatch("Pippo", "Pluto");
+  }
+
   public function test_that_true_is_true()
   {
     $this->assertTrue(true);
   }
 
-  public function test_can_create_new_TableTennisMatch()
+  public function test_print_status_of_started_match()
   {
-    $m = new TableTennisMatch("Pippo", "Pluto");
+    $this->initMatch("Pippo", "Pluto");
+
+    $this->assertEquals("[0 - 0] Ball to Pippo", $this->match->printStatus());
   }
 
-  /*public function test_print_status_of_started_match()
+  public function test_assign_first_point_should_print_new_result()
   {
-    $m = new TableTennisMatch("Pippo", "Pluto");
-    $this->assertEquals("[0 - 0] Ball to Pippo", $m->printStatus());
-  }*/
+    $this->initMatch("Pippo", "Pluto");
+    $this->match->point("Pippo");
+
+    $this->assertEquals("[1 - 0] Ball to Pippo", $this->match->printStatus());
+  }
+
+  public function test_assign_some_points_should_print_correct_result()
+  {
+    $this->initMatch("Pippo", "Pluto");
+    $this->match->point("Pippo");
+    $this->match->point("Pippo");
+    $this->match->point("Pluto");
+    $this->match->point("Pippo");
+
+    $this->assertEquals("[3 - 1] Ball to Pippo", $this->match->printStatus());
+  }
+  
+  public function test_after_five_points_change_next_server()
+  {
+    $this->initMatch("Pippo", "Pluto");
+    $this->match->point("Pippo");
+    $this->match->point("Pluto");
+    $this->match->point("Pluto");
+    $this->match->point("Pippo");
+    $this->match->point("Pluto");
+
+    $this->assertEquals("[2 - 3] Ball to Pluto", $this->match->printStatus());
+  }
 }
